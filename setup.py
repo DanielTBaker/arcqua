c10fdc470a0c84272baee389d55308e5723a6cb2
Daniel Baker <dbaker@asiaa.sinica.edu.tw>
2024-01-10 15:46:26 +0800

    conifigured setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 07:30:00 2024
+
+@author: DanielTBaker
+"""
+
+from setuptools import setup
+
+setup(
+    name='arcqua',
+    version='0.2',
+    description='Tools for measuring ocean wave directions from CYGNSS and TRITON data',
+    long_description="See: `github.com/DanielTBaker/arcqua \
+                      <https://github.com/DanielTBaker/arcqua>`_.",
+    classifiers=[
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='DDMs, CYGNSS, scintillation',
+    url='https://github.com/DanielTBaker/arcqua',
+    author='Daniel T. Baker',
+    author_email='dbaker@asiaa.sinica.edu.tw',
+    license='MIT',
+    packages=['arcqua'],
+    install_requires=['numpy', 'scipy', 'matplotlib', 'astropy'],
+    include_package_data=True,
+    zip_safe=False,
+)
